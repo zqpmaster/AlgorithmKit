c5de23184370de02f9cb85a2a0b28a1351e7c929
zqpmaster <274430533@qq.com>
2014-10-10 11:21:09 +0800

    xiaomi

---
+++ XiaoMiWrittenQuestion.cpp
@@ -0,0 +1,9 @@
+//
+//  XiaoMiWrittenQuestion.cpp
+//  CppPractice
+//
+//  Created by ZQP on 14-10-10.
+//  Copyright (c) 2014年 ZQP. All rights reserved.
+//
+
+#include "XiaoMiWrittenQuestion.h"
---
+++ XiaoMiWrittenQuestion.h
@@ -0,0 +1,14 @@
+//
+//  XiaoMiWrittenQuestion.h
+//  CppPractice
+//
+//  Created by ZQP on 14-10-10.
+//  Copyright (c) 2014年 ZQP. All rights reserved.
+//
+
+#ifndef __CppPractice__XiaoMiWrittenQuestion__
+#define __CppPractice__XiaoMiWrittenQuestion__
+
+#include <stdio.h>
+
+#endif /* defined(__CppPractice__XiaoMiWrittenQuestion__) */
